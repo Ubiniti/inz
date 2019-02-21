d4bbbae1a2e649a7fd9c3471760618d70452da6a
Jay <michal.sidor3@pollub.edu.pl>
2019-02-21 12:31:17 +0100

    Update Video Playback

--- src/Controller/WatchController.php
+++ src/Controller/WatchController.php
@@ -12,8 +12,17 @@ class WatchController extends AbstractController
      */
     public function index()
     {
+        //id = hashed path
+        //get full path from DB entry with corresponding id
+        //get title,hashtags,etc. from DB
+        
+        $path="grill";
+        $title="grill";
+        
         return $this->render('watch/index.html.twig', [
             'controller_name' => 'WatchController',
+            'path' => $path,
+            'title' => $title
         ]);
     }
 }
