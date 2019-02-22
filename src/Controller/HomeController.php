a8996b4f9acbacdac0ff5da8287789ddaaab1128
Ubiniti <piotr.puchacz@onet.pl>
2019-02-22 11:13:16 +0100

    Show videos on homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Video;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,8 +14,18 @@ class HomeController extends AbstractController
      */
     public function index()
     {
+        $repository = $this->getDoctrine()->getRepository(Video::class);
+        $videos = $repository->findBy([], ['views' => 'DESC'], 20);
+        $videos_data = array();
+
+        for($i = 0; $i < count($videos); $i++)
+        {
+            $videos_data[$i] = $videos[$i]->toArray();
+        }
+        
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'videos' => $videos_data
         ]);
     }
 }
