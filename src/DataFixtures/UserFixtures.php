57a1668833cf6e45819a0d5061bc861ac585e585
Ubiniti <piotr.puchacz@onet.pl>
2019-02-22 14:59:22 +0100

    Add User and Video data fixtures
    Include data in CSV files and autoloaders (fixtures)

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $file = fopen('public/data/user.csv', "r");
+        while(! feof($file))
+        {
+            $data = fgetcsv($file);
+            $user = new User();
+            $user->setUsername($data[1]);
+            $user->setRoles(array($data[2]));
+            $user->setPassword($data[3]);
+            $user->setEmail($data[4]);
+            $user->setJoinDate(new \DateTime($data[5]));
+            $user->setCountry($data[6]);
+            $user->setBirthDate(new \DateTime($data[7]));
+            
+            $manager->persist($user);
+        }
+        fclose($file);
+
+        $manager->flush();
+    }
+}
---
+++ src/DataFixtures/VideoFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Video;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class VideoFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $file = fopen('public/data/video.csv', "r");
+        while(! feof($file))
+        {
+            $data = fgetcsv($file);
+            $video = new Video();
+            $video->setHash($data[8]);
+            $video->setTitle($data[1]);
+            $video->setAuthorId($data[2]);
+            $video->setUploaded(new \DateTime($data[3]));
+            $video->setViews($data[4]);
+            $video->setDescription($data[5]);
+            $video->setDuration(new \DateTime($data[6]));
+            $video->setCategory($data[7]);
+            
+            $manager->persist($video);
+        }
+        fclose($file);
+
+        $manager->flush();
+    }
+}
