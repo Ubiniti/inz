68ef45124b61614031e1593aa90b73fc3e9e3b51
Ubiniti <piotr.puchacz@onet.pl>
2019-02-22 14:08:51 +0100

    Make Comment Entity
    Comment entity is in relation with Video. Nesting comments avaliable through parrent_id

---
+++ src/Entity/Comment.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CommentRepository")
+ */
+class Comment
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $contents;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $author_id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $added;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $likes;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $dislikes;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $parrent_id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContents(): ?string
+    {
+        return $this->contents;
+    }
+
+    public function setContents(string $contents): self
+    {
+        $this->contents = $contents;
+
+        return $this;
+    }
+
+    public function getAuthorId(): ?int
+    {
+        return $this->author_id;
+    }
+
+    public function setAuthorId(int $author_id): self
+    {
+        $this->author_id = $author_id;
+
+        return $this;
+    }
+
+    public function getAdded(): ?\DateTimeInterface
+    {
+        return $this->added;
+    }
+
+    public function setAdded(\DateTimeInterface $added): self
+    {
+        $this->added = $added;
+
+        return $this;
+    }
+
+    public function getLikes(): ?int
+    {
+        return $this->likes;
+    }
+
+    public function setLikes(int $likes): self
+    {
+        $this->likes = $likes;
+
+        return $this;
+    }
+
+    public function getDislikes(): ?int
+    {
+        return $this->dislikes;
+    }
+
+    public function setDislikes(int $dislikes): self
+    {
+        $this->dislikes = $dislikes;
+
+        return $this;
+    }
+
+    public function getParrentId(): ?int
+    {
+        return $this->parrent_id;
+    }
+
+    public function setParrentId(?int $parrent_id): self
+    {
+        $this->parrent_id = $parrent_id;
+
+        return $this;
+    }
+}
