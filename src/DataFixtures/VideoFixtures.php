886f95c0e7560b63be0a37294b925ad3dd52a0bb
Ubiniti <piotr.puchacz@onet.pl>
2019-03-10 20:59:12 +0100

    Fix video fixture
    $data array indices were mismatched with row loaded from CSV file.

--- src/DataFixtures/VideoFixtures.php
+++ src/DataFixtures/VideoFixtures.php
@@ -17,16 +17,15 @@ class VideoFixtures extends Fixture
             if(empty($data))
                 break;
             $video = new Video();
-            $video->setTitle($data[1]);
-            $video->setUploaded(new \DateTime($data[2]));
-            $video->setViews($data[3]);
-            $video->setDescription($data[4]);
-            $video->setDuration(new \DateTime($data[5]));
-            $video->setCategory($data[6]);
-            //$video->setHash($data[7]);
-            $video->setThumbsUp($data[8]);
-            $video->setThumbsDown($data[9]);
-            $video->setAuthorUsername($data[10]);
+            $video->setTitle($data[2]);
+            $video->setAuthorUsername($data[3]);
+            $video->setUploaded(new \DateTime($data[4]));
+            $video->setViews($data[5]);
+            $video->setDescription($data[6]);
+            $video->setDuration(new \DateTime($data[7]));
+            $video->setCategory($data[8]);
+            $video->setThumbsUp($data[9]);
+            $video->setThumbsDown($data[10]);
             $video->generateHash();
             
             $manager->persist($video);
