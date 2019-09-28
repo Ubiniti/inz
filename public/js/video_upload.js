d74802ac021f108cfec39fb0ba17ddd374fb694e
Ubiniti <piotr.puchacz@onet.pl>
2019-09-28 19:38:22 +0200

    Add upload progress bar

---
+++ public/js/video_upload.js
@@ -0,0 +1,41 @@
+(function () {
+    $(document).ready(function () {
+        var progressBar = $('#upload-progress');
+
+        function progressHandling(e) {
+            var percent = e.loaded / e.total * 100;
+
+            progressBar.css('width', percent + '%');
+        }
+
+        $(uploadForm).on('submit', function(e) {
+            e.preventDefault();
+
+            var formData = new FormData(uploadForm);
+            formData.append('add_video[file]', videoFile.files[0]);
+
+            $.ajax({
+                url: $(this).action,
+                type: 'POST',
+                data: formData,
+                contentType: false,
+                cache: false,
+                processData:false,
+                xhr: function() {
+                    var myXhr = $.ajaxSettings.xhr();
+                    if (myXhr.upload) {
+                        myXhr.upload.addEventListener('progress', progressHandling, false);
+                    }
+                    return myXhr;
+                },
+                success: function(data) {
+                    console.log('Success uploading');
+                },
+                eroor: function(error) {
+                    console.log('Error:');
+                    console.log(error);
+                }
+            })
+        })
+    })
+})();
